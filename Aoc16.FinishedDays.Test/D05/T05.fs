bfe7fbaabc06509c6bb74e676b16fd654df5d194
Blaž Peterlin <blasius.peterlin@gmail.com>
2022-10-10 22:38:55 +0200

    Day 5

---
+++ Aoc16.FinishedDays.Test/D05/T05.fs
@@ -0,0 +1,25 @@
+﻿module Aoc16.FinishedDays.Test.T05
+open Aoc16.D05
+
+open Aoc16.Input
+open NUnit.Framework
+open System
+
+[<SetUp>]
+let Setup () =
+    ()
+
+let basePath = System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name.Replace("T","D")
+let prependFolder fname = basePath + "\\" + fname
+    
+[<TestCase("input-TEST.txt", "1a3099aa")>] 
+let Test1 (fn : string, res: string) = 
+    let input = fn |> prependFolder |> f2text
+    let sln1 = solve1 input
+    Assert.That(sln1, Is.EqualTo res)
+    
+[<TestCase("input-TEST.txt", "694190cd")>] 
+let Test2 (fn : string, res: string) = 
+    let input = fn |> prependFolder |> f2text
+    let sln2 = solve2 input
+    Assert.That(sln2, Is.EqualTo res)
---
+++ Aoc16.FinishedDays/D05.fs
@@ -0,0 +1,61 @@
+﻿module Aoc16.D05
+
+open Aoc16
+open Input
+open TextCopy
+open System.Collections.Generic
+open System.Security.Cryptography
+open Aoc16.Operators
+open System.Text
+
+let input =  "input.txt" |> f2text
+
+let parseLine (ln:string) =
+    ln
+    // |> text2tokens "x"
+    // |> text2tokensStr ["abc";"def"]
+
+let parse2lines (text:string) = 
+    text
+    |> text2lines 
+    |> List.map parseLine
+
+
+let md5 (dataStr : string) : string =
+    let data = System.Text.Encoding.ASCII.GetBytes(dataStr)
+    use md5 = MD5.Create()
+    (StringBuilder(), md5.ComputeHash(data))
+    ||> Array.fold (fun sb b -> sb.Append(b.ToString("x2")))
+    |> string
+
+let solve1 (text:string) = 
+    let inp = text |> parse2lines |> head
+    Seq.initInfinite id
+    |> Seq.map (fun i -> inp + i.ToString())
+    |> Seq.map md5
+    |> Seq.filter (fun r -> r.StartsWith "00000")
+    |> Seq.map (fun str -> str[5].ToString())
+    |> Seq.take 8
+    |> Seq.reduce (+)
+    
+let solve2 (text:string) = 
+    let inp = text |> parse2lines |> head
+    Seq.initInfinite id
+    |> Seq.map (fun i -> inp + i.ToString())
+    |> Seq.map md5
+    |> Seq.filter (fun r -> r.StartsWith "00000")
+    |> Seq.filter (fun str -> match System.Int32.TryParse (str[5].ToString()) with | true,i -> i >= 0 && i <= 7 | _ -> false)
+    |> Seq.map (fun str -> (str[5].ToString() |> int, str[6].ToString()))
+    |> Seq.distinctBy fst
+    |> Seq.take 8
+    |> Seq.sortBy fst
+    |> Seq.map snd
+    |> Seq.reduce (+)
+
+//let res1 = input |> solve1
+let res2 = input |> solve2
+
+ClipboardService.SetText(res2.ToString())
+
+let finished = true
+()
